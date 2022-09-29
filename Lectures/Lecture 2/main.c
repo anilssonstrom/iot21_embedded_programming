fc454e114906edc03c8db07c6c1a0bdceabdccde
Andreas Nilsson Ström <andreas.nilsson@natrolit.se>
2022-09-29 14:01:48 +0200

    Update lecture 2

--- Lectures/Lecture 2/main.c
+++ Lectures/Lecture 2/main.c
@@ -1,8 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+    //char name[8] = {'A', 'n', 'd', 'r', 'e', 'a', 's'};
+    char name[] = "Andreas";
+    printf("%s\n", name);
+    printf("%d\n", strlen(name));
+
+/*
+    //int values[] = {1, 2, 3, 4, 5};
+    int values[1];
+
+    values[0] = 10;
+    values[1] = 11;
+    values[2] = 12;
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d\n", values[i]);
+    }
+*/
+
+/*
+    // Skapa variabler
+    const int SIZE_TABLESPOON = 15;
+    const int SIZE_TEASPOON = 5;
+    int input, tablespoons, teaspoons, remainder;
+
+    // Be användaren om input
+    printf("Enter volume to convert (ml): ");
+    scanf("%d", &input);
+
+    // Konvertera till matskedar
+    tablespoons = input / SIZE_TABLESPOON;
+
+    // Konvertera till teskedar
+    // 37 från början
+    // Ta bort 30 ml (2 matskedar)
+    // 7cl kvar, gör till teskedar
+    //teaspoons = (input - (tablespoons * SIZE_TABLESPOON)) / SIZE_TEASPOON;
+    teaspoons = (input % SIZE_TABLESPOON) / SIZE_TEASPOON;
+
+    // Räkna ut resten (kryddmått)
+    //remainder = input - (tablespoons * SIZE_TABLESPOON) - (teaspoons * SIZE_TEASPOON);
+    remainder = input % SIZE_TEASPOON;
+
+    // Skriv ut allt
+    printf("Matskedar : %d\n", tablespoons);
+    printf("Teskedar  : %d\n", teaspoons);
+    printf("Kryddmått : %d\n", remainder);
+*/
+/*
+    // FizzBuzz
+
+    for (int i=1; i <= 30; i++)
+    {
+        if (i % 15 == 0)
+        {
+            printf("FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else
+        {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+*/
+/*
+    // Exempel: Break och continue
+    for (int i = 0; i <= 10; i++)
+    {
+        if (i == 4)
+        {
+            continue;
+        }
+
+        printf("%d\n", i);
+    }
+
+    int i = 0;
+    while (1) {
+        printf("%d\n", i);
+        i++;
+
+        if (i == 10)
+        {
+            break;
+        }
+    }
+*/
+/*
     // While-loop: Skriv ut 1-10
 
     int while_counter = 1;
@@ -19,7 +116,7 @@ int main()
     {
         printf("%d\n", counter);
     }
-
+*/
 /*
     // Exempel på switch-case
 
